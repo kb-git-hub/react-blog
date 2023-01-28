0f33498d1116264cc3470bbc763033cfebb1d1a3
KB <kj.br@protonmail.com>
2023-01-28 13:26:25 -0600

    Title Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div className="App">
-      <Header title='React Posts Pages'/>
+      <Header title='React Message Board'/>
       <Nav />
       <Outlet/>
       <Footer/>
