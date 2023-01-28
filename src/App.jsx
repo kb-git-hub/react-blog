bdf85bee3e7361a62e9880b8831073fea4ba9c43
KB <kj.br@protonmail.com>
2023-01-27 22:05:53 -0600

    Use Context Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import {Route, Routes, useNavigate} from 'react-router-dom'
 import { format } from "date-fns"
+import { DataProvider } from './context/DataContext'
 
 import Home from './Home'
 import NewPost from './NewPost'
@@ -29,26 +30,6 @@ function App() {
   useEffect(()=>{
     setPosts(data)
   }, [data])
-  // useEffect(()=>{
-  //   const fetchPosts = async () =>{
-  //     try{
-  //       const response = await api.get('/posts')
-  //       setPosts(response.data)
-
-  //     } catch (err){
-
-  //       if (err.response){
-  //         // if not in 200 response range
-  //         console.log(err.response.data);
-  //         console.log(err.response.status);
-  //         console.log(err.response.headers);
-  //       } else console.log(`Error: ${err.message}`)
-  //     }
-  //   }
-
-  //   fetchPosts()
-
-  // },[])
 
   useEffect(()=>{
     const filteredResults = posts.filter(post=>
